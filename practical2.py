72da56b73cb9ab97facd345b53782a8a4a6c7910
Vaibhav Shivaji Wabale <57138983+vaibhavwabale@users.noreply.github.com>
2022-11-15 13:44:05 +0530

    create practical2.py

---
+++ practical2.py
@@ -0,0 +1,29 @@
+1. ChatBot 
+
+dict = {
+    "hi": "Hello",
+    "Who are you ?": "I am ChatBot",
+    "What is your name ?": "My name is Vaibhav",
+    "What is the name of your institute ?": "ICCS",
+    "Percentage in Graduation ?": "76%"
+}
+
+a = input()
+
+if a in dict:
+    print(dict[a])
+else:
+    print("Invalid Input, I don't know the answer !!")
+   
+2. Area of Square
+def area():
+    a = int(input("Enter Side"))  
+    area = a * a 
+    print(area)
+ 
+3. Area of Triangle
+def triangle():
+    base = int(input("Enter Base"))
+    height = int(input("Enter Height"))
+    areaTri = 0.5 * base * height
+    print(areaTri)
